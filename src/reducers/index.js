1394c27357b2e2b118ea0372020bff9199f5d038
Bre Thunholm <bthunholm@gmail.com>
2021-06-09 09:09:17 -0500

    intialized song reducer component

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+const songsReducer = () => [
+  { title: 'Oops!...I Did it Again', duration: '4:05' },
+  { title: 'Womanizer', duration: '2:30' },
+  { title: 'Toxic', duration: '2:05' },
+  { title: 'Stronger', duration: '3:00' },
+  { title: 'Slave For You', duration: '1:45' },
+];
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+  return selectedSong;
+};
